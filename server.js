d4096fc625b77a738eb5b5a28a44dfdeaedb0077
akgurd <deniz.akgur@hva.nl>
2023-05-15 13:01:10 +0200

    modified

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const express = require('express')
-const app = express()
+const { engine } = require('express-handlebars');
+const http = require('http').createServer(app);
+const PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-app.listen(3000)
+app.use('/static', express.static('static'));
+app.use('/js', express.static('js'));
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+   
+    app.get('/', function(req, res) {
+      res.render('home', { title: 'Homepage' });
+    });
+    
+
+
+    http.listen(PORT, () => {
+      console.log(`Server gestart on port ${PORT}`);
+    });
